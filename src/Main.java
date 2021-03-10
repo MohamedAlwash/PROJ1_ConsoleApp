74b11724253e12cc8598a6d7fd06440ba5f4f07a
richalljangi <richall-jangi@hotmail.com>
2021-03-10 15:24:27 +0100

    Realisatie van menu

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,54 @@
 package src;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 class Main
 {
-	public static void main(String[] args)
-  {
+	public static void main(String[] args){
     System.out.println("Hallo World!");
-  }
+    int selectie = Menu();
+    switch (selectie){
+        case 1:
+            System.out.println("keuze 1");
+            break;
+        case 2:
+            System.out.println("keuze 2");
+            break;
+        case 3:
+            System.out.println("keuze 3");
+            break;
+        case 4:
+            System.out.println("keuze 4");
+            break;
+        case 5:
+            System.out.println("keuze 5");
+            break;
+        case 6:
+            System.out.println("keuze 6");
+            break;
+        case 7:
+            System.out.println("keuze 7");
+            break;
+
+    }
+}
+
+    public static int Menu(){
+	    int selectie;
+	    Scanner scanner = new Scanner(System.in);
+        ArrayList<String> menuOpties = new ArrayList<>();
+        menuOpties.add("1) Lijst met examens");
+        menuOpties.add("2) Lijst met studenten");
+        menuOpties.add("3) Nieuwe studenten ");
+        menuOpties.add("4) Student verwijderen");
+        menuOpties.add("5) Examen afnemen");
+        menuOpties.add("6) Welke student heeft de meeste examens gehaald?");
+
+        for(String opties :menuOpties){
+            System.out.println(opties);
+        }
+
+        System.out.println("Maak een keuze");
+        selectie = scanner.nextInt();
+        return selectie;
+    }
 }
