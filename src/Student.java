6fb34df9374f7cd567569145d62d74c5633e129c
Ashwin Rhebok <ashwinrhebok4@gmail.com>
2021-03-10 15:28:57 +0100

    added student class

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+package src;
+
+class Student {
+
+    private String name;
+    private Integer studentNumber = 10000000;
+    private ArrayList<Exam> makes = new ArrayList<Exam>();
+    public static final ArrayList<Student> ALL_STUDENTS = new ArrayList<>();
+
+    public Student(String name) {
+        this.name = name;
+        ALL_STUDENTS.add(this);
+    }
+
+    private Integer createStudentNumber() {
+        studentNumber = studentNumber;
+        return studentNumber++;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getStudentNumber() {
+        return this.studentNumber;
+    }
+}
