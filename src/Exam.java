80684798ec956aded2915dede04e070bf4f90b8c
BakedCheese <48770243+BakedCheese@users.noreply.github.com>
2021-03-11 15:31:02 +0100

    Add files via upload
    Exam Class

---
+++ src/Exam.java
@@ -0,0 +1,41 @@
+package src;
+
+import java.util.ArrayList;
+
+public class Exam {
+
+    private String typeExam;
+    private ArrayList<Boolean> results = new ArrayList<Boolean>();
+
+    private ArrayList<Question> getMathQuestions(){
+        ArrayList<Question> mathQuestions = new ArrayList<Question>();
+
+        //Questions Math
+
+        mathQuestions.add(new Question("1 + 1 = ", "5" ));
+        mathQuestions.add(new Question("9 + 10 =", "21" ));
+
+
+
+
+        return mathQuestions;
+    }
+
+
+
+
+    public ArrayList<Question> getExam (String examType){
+
+        switch (examType){
+            case "math":
+                return getMathQuestions();
+        }
+
+        return null;
+    }
+
+    public String getTypeExam(){
+        return typeExam;
+    }
+
+}
