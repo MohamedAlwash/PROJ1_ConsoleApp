375aaea2029aa6e9c9bbf1df4fbe67f177778a37
GU1LT <77740294+GU1LT@users.noreply.github.com>
2021-03-26 14:18:51 +0100

    fixed english questions

--- src/Exams/EnglishExam.java
+++ src/Exams/EnglishExam.java
@@ -18,15 +18,15 @@ public class EnglishExam extends Exam {
 
         //Questions English
         englishQuestions.add(new Question("Is it Ananas or Annenenas?", "pineapple" ));
-        englishQuestions.add(new Question("Hoe zeg 'zwijn' in het engels?", "boar" ));
-        englishQuestions.add(new Question("Hoe zeg je 'hallo' in het engels?", "hello" ));
-        englishQuestions.add(new Question("Hoe zeg 'kameel' in het engels?", "camel" ));
-        englishQuestions.add(new Question("Hoe noem je een 'snelweg' in het engels?", "highway" ));
+        englishQuestions.add(new Question("Hoe zeg 'zwijn' in het Engels?", "boar" ));
+        englishQuestions.add(new Question("Hoe zeg je 'hallo' in het Engels?", "hello" ));
+        englishQuestions.add(new Question("Hoe zeg 'kameel' in het Engels?", "camel" ));
+        englishQuestions.add(new Question("Hoe noem je een 'snelweg' in het Engels?", "highway" ));
         englishQuestions.add(new Question("Geef een synoniem voor 'vicinity'", "environment" ));
-        englishQuestions.add(new Question("Geef een synoniem voor 'mountain'", "berg" ));
-        englishQuestions.add(new Question("Geef een synoniem voor 'redundant'", "overbodig" ));
-        englishQuestions.add(new Question("Hoe zeg je 'persectief' in het engels?", "perspective" ));
-        englishQuestions.add(new Question("Hoe zeg je 'dromedaris' in het engels?", "dromedary" ));
+        englishQuestions.add(new Question("Wat is 'mountain' in het Nederlands?", "berg" ));
+        englishQuestions.add(new Question("Wat is 'redundant' in het Nederlands?", "overbodig" ));
+        englishQuestions.add(new Question("Hoe zeg je 'persectief' in het Engels?", "perspective" ));
+        englishQuestions.add(new Question("Hoe zeg je 'dromedaris' in het Engels?", "dromedary" ));
 
 
 
